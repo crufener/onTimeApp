0e604e64f792a484837c897f246cbed82ced85e5
Craig Rufener <crufener@outlook.com>
2016-10-05 10:38:14 -0700

    initial project setup

---
+++ ontime/ngApp/controllers/controllers.ts
@@ -0,0 +1,16 @@
+namespace ontime.Controllers {
+
+    export class HomeController {
+        public message = 'Hello from the home page!';
+    }
+
+
+    export class AboutController {
+        public message = 'Hello from the about page!';
+    }
+
+    export class SignIn {
+        public message = 'This will be the sign in page to access your locations and schedule';
+    }
+
+}
